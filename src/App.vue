2148b0ead9837ed3e62ec5ab2dbd6406fcd9d604
Daniel Abernathy <dabernathy89@gmail.com>
2025-02-18 13:11:50 -0600

    update dependencies and change prompt

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
-<!-- Fetch the contents of https://api.publicapis.org/entries and display the first 10 entries in a table. Once the table is working, visually indicate odd rows. -->
+<!--
+Using the Disney API (https://disneyapi.dev), accomplish the following:
+* Fetch and display the first 10 characters from the API in a table.
+* The table should contain:
+  * thumbnail image
+  * the character's name (linked to their wiki page)
+  * the total number of their of film appearances
+* Create and use a TypeScript type for the character data.
+-->
 
 <script setup lang="ts">
 </script>
